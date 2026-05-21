edc8ba3555adc36106a6f613de71158dc06bf6cf
n3ck-hurt <202380085@psu.palawan.edu.ph>
2026-05-21 19:44:54 +0800

    completed full baseline organization crud lifecycle features setup

--- projectsite/studentorg/urls.py
+++ projectsite/studentorg/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import HomePageView, OrganizationList, OrgMemberListView, StudentListView, CollegeListView, ProgramListView, OrganizationCreateView, OrganizationUpdateView
+from .views import HomePageView, OrganizationList, OrgMemberListView, StudentListView, CollegeListView, ProgramListView, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
     path('members/', OrgMemberListView.as_view(), name='orgmember-list'),
     path('students/', StudentListView.as_view(), name='student-list'),
     path('colleges/', CollegeListView.as_view(), name='college-list'),
--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -41,6 +41,11 @@ class OrganizationUpdateView(UpdateView):
     template_name = 'org_form.html' 
     success_url = reverse_lazy('organization-list') 
 
+class OrganizationDeleteView(DeleteView): 
+    model = Organization 
+    template_name = 'org_del.html' 
+    success_url = reverse_lazy('organization-list') 
+
 class OrgMemberListView(ListView):
     model = OrgMember
     context_object_name = 'members'
